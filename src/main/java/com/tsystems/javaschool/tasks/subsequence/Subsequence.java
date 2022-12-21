d85680e90383e5798bfc80f6ce2be771d0cb7c3a
Nikita Konovalov Shishov <akonovalovni2011@gmail.com>
2022-12-21 13:23:48 +0000

    Subsequence.java edited online with Bitbucket

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -14,7 +14,33 @@ public class Subsequence {
      */
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+
+        if (x == null && y.isEmpty())
+            throw new IllegalArgumentException();
+        if (y == null && x.isEmpty())
+            throw new IllegalArgumentException();
+
+        if (x.equals(y))
+            return true;
+
+        if (x == null || y == null)
+            return false;
+
+        if (x.size() < y.size()) {
+            int index = 0;
+            for (int i = 0; i < x.size(); i++) {
+
+                if (y.subList(index, y.size() - 1).indexOf(x.get(i)) != -1) {
+
+                    index = y.subList(index, y.size() - 1).indexOf(x.get(i)) + 1;
+
+                } else
+                    return false;
+
+            }
+            return true;
+        } else
+            return false;
+
     }
 }
